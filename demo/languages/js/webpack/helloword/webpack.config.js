3fb0f7585bc9e4a02b32c8e4bda0354cdd05ed32
Lei Cao <lei.cao.life@gmail.com>
2018-08-15 17:15:31 +0800

    webpack helloworld

--- demo/languages/js/webpack/helloword/webpack.config.js
+++ demo/languages/js/webpack/helloword/webpack.config.js
@@ -1,9 +1,35 @@
-const path = require('path');
+var fs    = require("fs")
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+         app: './src/index.js',
+         print: './src/print.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: './dist',
+        before: function(app) {
+            app.get('/key.cert', function(req, res) {
+                res.sendFile('/Users/l.cao/programming/cert/localhost+4-key.pem');
+            });
+        },
+        host: 'lei.dev',
+        https: {
+            key: fs.readFileSync('/Users/l.cao/programming/cert/localhost+4-key.pem'),
+            cert: fs.readFileSync('/Users/l.cao/programming/cert/localhost+4.pem')
+        }
     }
 };
